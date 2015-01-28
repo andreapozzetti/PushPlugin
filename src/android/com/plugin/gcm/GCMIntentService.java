e1faa6e01fd2e17b70236609ad3a075bbbd09582
andrea pozzetti <andrepozz@inwind.it>
2015-01-28 12:01:37 +0100

    Sound Notification

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -7,6 +7,8 @@ import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -119,6 +121,15 @@ public class GCMIntentService extends GCMBaseIntentService {
 		if (msgcnt != null) {
 			mBuilder.setNumber(Integer.parseInt(msgcnt));
 		}
+        
+        
+        String soundName = extras.getString("sound");
+        if (soundName != null) {
+            Resources r = getResources();
+            int resourceId = r.getIdentifier(soundName, "raw", context.getPackageName());
+            Uri soundUri = Uri.parse("android.resource://" + context.getPackageName() + "/" + resourceId);
+            mBuilder.setSound(soundUri);
+        }
 		
 		int notId = 0;
 		
